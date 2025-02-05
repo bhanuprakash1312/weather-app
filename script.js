f6f37c1aa03756f0dd50fd765a3355195d58e7fa
bhanuprakash1312 <bp136897@gmail.com>
2025-02-05 11:06:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,18 @@ const searchbut = document.querySelector(".form button");
 const weathericon = document.querySelector(".weather-icon");
 
 async function checkweather(city) {
-    const response = await fetch(apiUrl + city + `&appid=${apikey}`);
-    var data = await response.json();
-    console.log(data);
-    document.querySelector(".city").innerHTML = data.name;
+  try{
+        const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+        if(!response.ok){
+            throw new Error(`HTTP Error! Status: ${response.status} (${response.statusText}`);
+        }
+        
+        var data = await response.json();
+        if (!data || !data.main || !data.weather) {
+            throw new Error("Invalid API response structure");
+        }
+        console.log(data);
+        document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
@@ -30,6 +38,15 @@ async function checkweather(city) {
     else if(data.weather[0].main === "Mist"){
         weathericon.src = "mist.png"
     }
+
+}
+    
+    catch(error){
+        console.error("Error fetching weather data:", error.message);
+        alert(`Failed to fetch weather data: ${error.message}`);
+        return null
+    }
+   
 
 }
 searchbut.addEventListener("click",()=>{
